c943c0324a0e601903b85798a4c09551d688510c
brunokian <brunokian@hotmail.com>
2023-12-13 18:06:35 -0300

    fazendo sidebar

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next'
+import { Figtree } from 'next/font/google'
+import './globals.css'
+import Sidebar from './components/Sidebar'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'MusicApp',
+  description: 'Create by Bruno Kian',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Sidebar>
+
+          {children}
+        </Sidebar>
+      </body>
+    </html>
+  )
+}
